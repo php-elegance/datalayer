82235d3c3a64032327c4e03b9039935cfbf3d97c
André Ricardo <contatoadmx@gmail.com>
2023-10-22 02:06:20 -0300

    implement storage path

--- class/Elegance/Datalayer/Connection/Sqlite.php
+++ class/Elegance/Datalayer/Connection/Sqlite.php
@@ -20,7 +20,7 @@ class Sqlite extends Connection
 
         $file = File::setEx($file, 'sqlite');
 
-        $this->data['file'] = path('source/sqlite', $file);
+        $this->data['file'] = path('storage/sqlite', $file);
 
         $this->instancePDO = ["sqlite:" . $this->data['file']];
     }
