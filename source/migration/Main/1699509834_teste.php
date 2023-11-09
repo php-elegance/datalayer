5eb7b98f1791bb1169544be21a1a1a20496885cf
André Ricardo <contatoadmx@gmail.com>
2023-11-09 18:35:33 -0300

    change path name

---
+++ source/migration/Main/1699509834_teste.php
@@ -0,0 +1,13 @@
+<?php
+
+/** Migration 1699509834_teste */
+new class($dbName, $mode) extends Elegance\Datalayer\Migration
+{
+    function up()
+    {
+    }
+
+    function down()
+    {
+    }
+};
--- src/helper/script/path.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Elegance\Core\Path;
-
-Path::alias('elegance-datalayer', dirname(__DIR__, 3));
