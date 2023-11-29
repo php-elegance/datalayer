81712079bab7467b4f2ceb0d48e3a937b8b68e96
André Ricardo <contatoadmx@gmail.com>
2023-11-29 01:35:47 -0300

    change syncids

--- helper/function/syncIds.php
+++ helper/function/syncIds.php
@@ -6,9 +6,9 @@ use Elegance\Datalayer\Driver\Table;
 if (!function_exists('syncIds')) {
 
     /** Sincroniza dois campos com IDSs cruzados */
-    function syncIds(Record $record, string $fieldFrom, array $newValues, Table $table, string $fieldTo): void
+    function syncIds(int $recordId, array $oldValues, array $newValues, Table $table, string $fieldName): void
     {
-        $old = $record->{$fieldFrom}->get();
+        $old = $oldValues;
         $new = $newValues;
 
         $add = [];
@@ -26,19 +26,14 @@ if (!function_exists('syncIds')) {
 
         foreach ($add as $id) {
             $tableRecord = $table->getOne($id);
-            $tableRecord->{$fieldTo}->add($record->id());
+            $tableRecord->{$fieldName}->add($recordId);
             $tableRecord->_save();
         }
 
         foreach ($remove as $id) {
             $tableRecord = $table->getOne($id);
-            $tableRecord->{$fieldTo}->remove($record->id());
+            $tableRecord->{$fieldName}->remove($recordId);
             $tableRecord->_save();
         }
-
-        $record->{$fieldFrom}->set($newValues);
-        $record->_save();
-
-        ddpre($add, $remove, count($table->getAll('id', [...$add, ...$remove])));
     }
 }
